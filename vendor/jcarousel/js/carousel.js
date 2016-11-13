12c756cf83fa30f4b6d31204302da4ccfd18d0d8
Sergei Dikarev <dikarevsergei@gmail.com>
2016-11-13 23:14:13 +0200

    One

---
+++ vendor/jcarousel/js/carousel.js
@@ -0,0 +1,32 @@
+(function($) {
+    $(function() {
+        var jcarousel = $('.jcarousel');
+
+        jcarousel
+            .on('jcarousel:reload jcarousel:create', function() {
+                var carousel = $(this),
+                    width = carousel.innerWidth();
+
+                if (width >= 940) {
+                    width = width / 4;
+                } else if (width >= 350) {
+                    width = width / 2;
+                }
+
+                carousel.jcarousel('items').css('width', Math.ceil(width) + 'px');
+            })
+            .jcarousel({
+                wrap: 'circular'
+            });
+
+        $('.jcarousel-control-prev')
+            .jcarouselControl({
+                target: '-=1'
+            });
+
+        $('.jcarousel-control-next')
+            .jcarouselControl({
+                target: '+=1'
+            });
+    });
+})(jQuery);
